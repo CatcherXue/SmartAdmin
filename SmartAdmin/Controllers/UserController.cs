6d68c94f399ac731ffd021e7254f3bf810622c71
wangya <smartbooks@qq.com>
2012-12-05 13:14:22 +0800

    开发用户管理功能模块
    为开发完成，下次需要接着开发。预计2个工作日时间左右。

--- SmartAdmin/Controllers/UserController.cs
+++ SmartAdmin/Controllers/UserController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Smart.Admin;
 
 namespace Smart.Admin.Controllers
 {
+    using Smart.Admin;
     /// <summary>
     /// 用户管理
     /// 网站管理员、职员、会员账户管理。
@@ -22,8 +24,26 @@ namespace Smart.Admin.Controllers
             return View();
         }
 
-        [HttpPost]
+        [HttpGet]
         public ActionResult Add()
+        {
+            Models.User entity = new Models.User();
+            entity.CurrentIPAddress = this.Request.UserHostAddress;
+            entity.EmailAddress = "";
+            entity.EndLoginDate = DateTime.Now;
+            entity.MutileOnLine = false;
+            entity.Nick = "";
+            entity.OnLine = false;
+            entity.Password = "";
+            entity.RegisterDate = DateTime.Now;
+            entity.UserName = "";
+            entity.UserRole = "";
+
+            return View(entity);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Models.User model)
         {
             return View();
         }
